366e96e037114d4c3fada6e04daa41e4564fefe5
Administrator <Administrator@DESKTOP-FU2AS1G>
2022-07-26 14:07:09 +0900

    0725

--- src/Ch13Static/C05SingtonMain.java
+++ src/Ch13Static/C05SingtonMain.java
@@ -7,6 +7,17 @@ class Product
 	String PName;
 	int amount;
 	//모든 인자를 받아 저장하는 생성자를 만듭니다.
+	public Product(String pName, int amount) {
+		super();
+		PName = pName;
+		this.amount = amount;
+	}
+	
+	@Override
+	public String toString() {
+		return "Product [PName=" + PName + ", amount=" + amount + "]";
+	}
+	
 }
 class Refrigerator
 {
@@ -17,23 +28,68 @@ class Refrigerator
 		if(instance==null)
 		{
 			instance=new Refrigerator();
+			return instance;
 		}
-		return instance;
 	}
 	private Refrigerator() {}
+	
+	
 		
 	Product [] list = new Product[100];
 	int ProductNum = 0;
+	
 	void SetProduct(Product product) {
+		if(ProductNum<=99) {
 		//list  ProductNum번째 idx에 외부에서 생성된 Product가 연결
-		
+		list[ProductNum]=product;
 		//ProductNum의 count 1증가
+		ProductNum++;
+		}
+		else
+		{
+			System.out.println("더 이상 담을 수 있는 공간이 없습니다");
+		}
 	}
-	Product getProduct(String search,int amount) {
-			
+	Product GetProduct(String search,int amount) {
+		
 		//반복문과 문자열 검색 처리를 통해서 list에 저장된 search 
 		//객체 꺼내와서 요청 개수만큼 차감
 		//찾은 Product의 주소를 리턴
+		
+		if(ProductNum != 0)
+		{
+			for(int i =0;i<ProductNum;i++) {
+				if(list[i].PName.equals(search))
+				{
+					//일치하는 제품이 있다면
+					if(list[i].amount==amount)
+					{
+						//재고량 == 요청수량
+						//list에서 제품삭제(자료구조..삭제처리 -> 컬렉션)
+						//return Product
+					}
+					else if(list[i].amount<amount)
+					{
+						//재고량 > 요청수량
+						//수량 감소
+						list[i].amount = list[i].amount-amount;
+						//Product 객체 생성
+						//Product 리턴
+						return new Product(list[i].PName,amount);
+					}
+				}
+			}
+		}
+		//일치하는 제품이 없다면
+		return null;
+		
+		//로직
+		//만약 제품이 검색 안된다면 null 리턴
+		//만약 제품이 검색 되고 수량이 저장된 수량을 초과한다면 null리턴
+		//만약 제품이 검색 되고 수량이 저장된 수량보다 작다면
+		//-Product객체를 따로 만들어 수량 분배하여 리턴
+		//만약 제품이 검색되고 수량이 저장된 수량과 같다면 list안의 내용물삭제후
+		//제품 리턴
 		return null;
 	}
 }
@@ -42,7 +98,7 @@ public class C05SingtonMain {
 
 	public static void main(String[] args) {
 		//냉장고 객체 가져오기(싱글톤 패턴)
-		Refrigerator refrigerator= Refregerator.getInstance();
+		Refrigerator refrigerator= Refrigerator.getInstance();
 				
 		//상품정보객체 생성 
 		Product prod = new Product("콜라",5);
@@ -51,7 +107,7 @@ public class C05SingtonMain {
 		refrigerator.SetProduct(prod);
 
 		//냉장고에 상품반환
-		Product prod1 =refrigerator.getProduct("사이다",1); 
+		Product prod1 =refrigerator.GetProduct("사이다",1); 
 						
 		System.out.println("get Item : " + prod1.toString());
 
